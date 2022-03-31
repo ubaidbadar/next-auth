d6ba5df1ec4254ec8408243e4fe93b1befdc614e
Ubaid Badarm <m.ubaidbadar@gmail.com>
2022-03-31 12:52:55 +0500

    updated

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
 import { AppProps } from 'next/app';
 import '../styles/index.css';
 import { SessionProvider } from 'next-auth/react';
-import Header from '../components/Header/Header';
 
 function MyApp({ Component, pageProps, ...props }: AppProps) {
   return (
